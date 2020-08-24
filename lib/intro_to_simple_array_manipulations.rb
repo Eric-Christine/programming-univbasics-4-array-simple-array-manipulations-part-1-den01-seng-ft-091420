9a69aa4558852fba91b299970b20e9d65f04b3b9
Eric Christine <ericjchristine@gmail.com>
2020-08-24 17:46:11 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+
+  def using_push(colors_in_the_rainbow, next_color)
+    colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+    next_color = "violet"
+    updated_array = colors_in_the_rainbow.push(next_color)
+  end
+
+def using_unshift(bouroughs_in_nyc, new_neighborhood)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  new_neighborhood = "Staten Island"
+  updated_array = bouroughs_in_nyc.unshift(new_neighborhood)
+end
+
+
+def using_pop()
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"] 
+  new_neighborhood = ["Staten Island"]
+  updated_array = continents.pop(new_neighborhood)
+end
+
+def pop_with_args()
+    
+  
